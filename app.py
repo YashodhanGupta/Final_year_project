ce5f95760082137ddd33e1679cb8b8d5eefe7ee9
YashodhanGupta <101509815+YashodhanGupta@users.noreply.github.com>
2025-02-17 22:51:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,11 +82,11 @@ working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # loading the saved models
 
-diabetes_model = pickle.load(open("D:\ML project\multiple-disease-prediction-streamlit-app-main\multiple-disease-prediction-streamlit-app-main\saved_models\diabetes_model.sav", 'rb'))
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open("D:\ML project\multiple-disease-prediction-streamlit-app-main\multiple-disease-prediction-streamlit-app-main\saved_models\heart_disease_model.sav", 'rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open("D:\ML project\multiple-disease-prediction-streamlit-app-main\multiple-disease-prediction-streamlit-app-main\saved_models\parkinsons_model.sav", 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
 # sidebar for navigation
 with st.sidebar:
